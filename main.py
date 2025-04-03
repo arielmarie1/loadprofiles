b2a40d0aee64a12357f62e48a544475259c986d7
arielmarie <arielstolz1@gmail.com>
2025-04-03 10:41:12 +0200

    Changing camp location to LJ and scaling loads accordingly.

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ load_dict = {}
 profile_df = pd.read_excel('profili.xlsx')
 
 # Add load profiles from excel file
-df, load_dict = generate_loads("Loads.csv", df, load_dict, NB_STEPS, STEPS_PER_DAY, profile_df,
+df, load_dict = generate_loads("Loads_LJ.csv", df, load_dict, NB_STEPS, STEPS_PER_DAY, profile_df,
                                rand_range=(0.9, 1.1))
 
 # Optional merge loads capability
@@ -150,15 +150,15 @@ df, load_dict = merge_loads(df, load_dict, merge_map)
 # Add vehicle data
 df, load_dict = generate_loads("Vehicles.csv", df, load_dict, NB_STEPS, STEPS_PER_DAY, profile_df)
 # Add data from renewables ninja
-df, load_dict = load_renewables("ninja_pv.csv", ["PV"], [2],
+df, load_dict = load_renewables("ninja_pv_LJ.csv", ["PV"], [2],
                                 df, load_dict, 1000000, load_type="Generation")
-df, load_dict = load_renewables("ninja_wind.csv", ["Wind"], [2],
+df, load_dict = load_renewables("ninja_wind_LJ.csv", ["Wind"], [2],
                                 df, load_dict, 1000, load_type="Generation")
-df, load_dict = load_renewables("ninja_demand.csv", ["Heating_Central", "Cooling_Central"], [3, 4],
+df, load_dict = load_renewables("ninja_demand_LJ.csv", ["Heating_Central", "Cooling_Central"], [3, 4],
                                 df, load_dict, 1000)
 
 # Add PERSEE required descriptive headers
 df = add_headers(df, load_dict, START_DATE)
 
 # Save final DataFrame
-df.to_csv("H2_dataseries_v2.csv", sep=";", index=False, header=False, float_format='%3g')
+df.to_csv("H2_dataseries_LJ.csv", sep=";", index=False, header=False, float_format='%3g')
