5aba286793581ab8ddb385996f015c5ba3b48bf7
arielmarie1 <arielstolz1@gmail.com>
2025-09-16 09:25:27 -0400

    Adds hp COP series and average values

--- temperatures.py
+++ temperatures.py
@@ -1,29 +1,33 @@
-import numpy as np
 import pandas as pd
+import hplib.hplib as hpl
 
 
 class Temperatures:
     def __init__(self, filename, coords: tuple[float, float]):
         self.lat = coords[0]
         self.lon = coords[1]
-        self.cop_temps = np.array([-15, -10, -5, 0, 5, 10, 15, 20], dtype=float)
-        self.cop_curves = {
-            1.0: np.array([2.80, 3.00, 3.20, 3.35, 3.45, 3.55, 3.62, 3.70]),
-            0.6: np.array([3.20, 3.35, 3.55, 3.70, 3.85, 4.00, 4.15, 4.30]),
-            0.3: np.array([3.45, 3.60, 3.85, 4.05, 4.25, 4.40, 4.50, 4.60]),
-        }
         self.filename = filename
-        self.cop_avg = None
 
+        self._hplib_temps()
         self._load_temp()
         self._avg_temp()
         self._seasonal_avg()
 
-    def cop_from_temp(self, temp_c_series, plr=0.6):
-        cops = self.cop_curves[plr]
-        x = np.asarray(temp_c_series, dtype=float)
-        x_clipped = np.clip(x, self.cop_temps.min(), self.cop_temps.max())
-        return np.interp(x_clipped, self.cop_temps, cops)
+    def _hplib_temps(self, t_out=60.0):
+        # Load database and parameters
+        database = hpl.load_database()
+        params = hpl.get_parameters(model="Generic", group_id=1, t_in=-7, t_out=52, p_th=10000)
+        # Create Generic Air/Water Heat Pump
+        hp = hpl.HeatPump(params)
+
+        sim = hp.simulate(
+            t_amb=self.df_temp['t2m'].values,
+            t_in_primary=self.df_temp['t2m'].values,
+            t_in_secondary=t_out,
+            mode=1
+        )
+        self.cop_series = pd.Series(sim["COP"])
+        self.cop_avg = pd.Series(sim["COP"]).mean()
 
     def _load_temp(self):
         try:
@@ -49,9 +53,3 @@ class Temperatures:
             self.summer_avg = winter["t2m"].mean()
             self.winter_avg = summer["t2m"].mean()
         return self.summer_avg, self.winter_avg
-
-
-LWS = Temperatures("re_ninja/ninja_weather_LWS.csv", coords=(65.44973126,27.18830351))
-HS = Temperatures("re_ninja/ninja_weather_HS.csv", coords=(-12.82155901,45.13898189))
-print(LWS.t_avg, LWS.winter_avg, LWS.summer_avg)
-print(HS.t_avg, HS.winter_avg, HS.summer_avg)
